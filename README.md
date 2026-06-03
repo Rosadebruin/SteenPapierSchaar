7bd05aa3a1ccbcb58751f40e677e8cb59b47c450
Rosadebruin <Rosa.de.Bruin@student.gildeopleidingen.nl>
2026-06-03 14:24:02 +0200

    Versie 1.2
    Final versie van de opdracht. README is ook ingevuld.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # SteenPapierSchaar
 Frontend Steen Papier Schaar opdracht Raymond
+
+Met deze gemaakte code door mij kun je met een computer steen papier schaar spelen. Hieronder staat de uitleg hoe je het runt:
+
+- Download allebei de files.
+- Ga naar je bestanden toe en zoek het bestand "index.html".
+- Dubbelklik op het bestand.
+- Nu is het bestand geopend en kan je het spelletje gaan spelen.
--- script.js
+++ script.js
@@ -5,23 +5,58 @@ const steenBtn = document.querySelector("#steen");
 const papierBtn = document.querySelector("#papier");
 const schaarBtn = document.querySelector("#schaar");
 
-let humanChoice ="";
+let humanChoice = "";
+let computerChoice = "";
 
-/*Dit zorgt ervoor dat de buttons linken aan wat je kiest*/
+/*Dit zorgt ervoor dat jouwn keuze als speler wordt bewaard*/
 
-steenBtn.addEventListener('click', function(event) {
+steenBtn.addEventListener('click', function (event) {
   humanChoice = event.target.id;
   humanOutput.innerHTML = humanChoice;
+  const randomNumber = Math.floor(Math.random() * 3) + 1;
+  if (randomNumber === 1) {
+    computerChoice = 'steen';
+  }
+  if (randomNumber === 2) {
+    computerChoice = 'schaar';
+  }
+  if (randomNumber === 3) {
+    computerChoice = 'papier';
+  }
+  computerOutput.innerHTML = computerChoice;
 });
 
-papierBtn.addEventListener('click', function(event) {
+
+papierBtn.addEventListener('click', function (event) {
   humanChoice = event.target.id;
   humanOutput.innerHTML = humanChoice;
+  const randomNumber = Math.floor(Math.random() * 3) + 1;
+  if (randomNumber === 1) {
+    computerChoice = 'steen';
+  }
+  if (randomNumber === 2) {
+    computerChoice = 'schaar';
+  }
+  if (randomNumber === 3) {
+    computerChoice = 'papier';
+  }
+  computerOutput.innerHTML = computerChoice;
 });
 
-schaarBtn.addEventListener('click', function(event) {
+schaarBtn.addEventListener('click', function (event) {
   humanChoice = event.target.id;
   humanOutput.innerHTML = humanChoice;
+  const randomNumber = Math.floor(Math.random() * 3) + 1;
+  if (randomNumber === 1) {
+    computerChoice = 'steen';
+  }
+  if (randomNumber === 2) {
+    computerChoice = 'schaar';
+  }
+  if (randomNumber === 3) {
+    computerChoice = 'papier';
+  }
+  computerOutput.innerHTML = computerChoice;
 });
 
 /*Dit verandert de HTML binnen het element. Dus de tekst die er zal komen te staan.*/
@@ -38,20 +73,20 @@ resultOutput.innerHTML = "result";
 
 steenBtn.addEventListener("click", function (event) {
   console.log("Klik event triggered: ", event);
-  alert("Klik event triggered");
+
 
 });
 
 
 papierBtn.addEventListener("click", function (event) {
   console.log("Klik event triggered: ", event);
-  alert("Klik event triggered");
+
 
 });
 
 
 schaarBtn.addEventListener("click", function (event) {
   console.log("Klik event triggered: ", event);
-  alert("Klik event triggered");
+
 
 });
