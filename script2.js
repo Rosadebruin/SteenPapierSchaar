40d4afedb53f96fa848f4e59e4f5ca8cc50a216c
Rosadebruin <Rosa.de.Bruin@student.gildeopleidingen.nl>
2026-06-23 14:56:12 +0200

    Event code en resultaat
    alle eventcodes zijn naar 1 gehele event code gezet en er is nu voor gezorgd dat je het resultaat kan zien wie heeft gewonnen.

---
+++ script2.js
@@ -0,0 +1,58 @@
+const humanOutput = document.querySelector("#human");
+const computerOutput = document.querySelector("#computer");
+const resultOutput = document.querySelector("#result");
+
+// selecteerd alle knoppen in 1 keer
+const knoppen = document.querySelectorAll("#steen, #papier, #schaar");
+
+let humanChoice = "";
+let computerChoice = "";
+
+// teksten result
+humanOutput.innerHTML = "Jouw keuze komt hier, maak je keuze!";
+computerOutput.innerHTML = "cp choice";
+resultOutput.innerHTML = "result";
+
+function speelSpel(event) {
+  // Logt het event
+  console.log("Klik event triggered: ", event);
+
+  // zorgt ervoor dat mijn keuze wordt bewaard
+  humanChoice = event.target.id;
+  humanOutput.innerHTML = humanChoice;
+
+  // genereert de keuze van de computer
+  const randomNumber = Math.floor(Math.random() * 3) + 1;
+  if (randomNumber === 1) {
+    computerChoice = 'steen';
+  } else if (randomNumber === 2) {
+    computerChoice = 'schaar';
+  } else if (randomNumber === 3) {
+    computerChoice = 'papier';
+  }
+  
+  // Toont de keuze van de computer
+  computerOutput.innerHTML = computerChoice;
+
+ // bepaalt de winnaar (gelijkspel)
+  if (humanChoice === computerChoice) {
+    resultOutput.innerHTML = "Gelijkspel!";
+  } 
+  // Alle keuzes wanneer ik win
+  else if (
+    (humanChoice === 'steen' && computerChoice === 'schaar') ||
+    (humanChoice === 'papier' && computerChoice === 'steen') ||
+    (humanChoice === 'schaar' && computerChoice === 'papier')
+  ) {
+    resultOutput.innerHTML = "Goedzo, jij hebt gewonnen! 🎉";
+  } 
+  // Als de computer wint
+  else {
+    resultOutput.innerHTML = "Helaas, de computer heeft gewonnen! 🤖";
+  }
+}
+
+// Loopt door alle knoppen heen en koppelt dezelfde functie eraan vast
+knoppen.forEach(knop => {
+  knop.addEventListener('click', speelSpel);
+});
